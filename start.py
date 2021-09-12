98348d9ecd58bf659fa5b6ec49779ea83e405829
Yuriy Trofimov <70945999+yytrofimov@users.noreply.github.com>
2021-09-12 07:27:27 +0300

    1) DEBUG, PORT const moved to environ

--- start.py
+++ start.py
@@ -6,4 +6,4 @@ sys.path.insert(0, './app/')
 if __name__ == '__main__':
     import __init__
 
-    __init__.app.run(host=os.environ.get('HOST'), debug=True)
+    __init__.app.run(host=os.environ.get('HOST'), debug=os.environ.get('DEBUG'), port=os.environ.get('PORT'))
